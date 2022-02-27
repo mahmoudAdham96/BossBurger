17728f1e55b33f4d38707e59fe7e2c919d31eba4
mahmoudAdham96 <88621466+mahmoudAdham96@users.noreply.github.com>
2022-02-28 00:07:24 +0200

    new

--- src/components/cards/prodCard.vue
+++ src/components/cards/prodCard.vue
@@ -4,7 +4,7 @@
             <card class=" bg-white  border-gray-100 rounded-lg  hover:shadow-lg align-center" v-for="prod in 20" :key="prod">
                 
                 <a href="">
-                <img src="../../assets/3.png" class="rounded-xl"/>
+                <img src="../../assets/3.png" class="rounded-xl" />
                 </a>
                 <p class="px-2 font-semibold pt-3 pb-2 capitalize flex justify-between"> <span>beef burger</span> <span class="text-orange-400">35.00 Rs</span> </p>                
                 <p class="px-2 py-2 mb-5 text-gray-500">
